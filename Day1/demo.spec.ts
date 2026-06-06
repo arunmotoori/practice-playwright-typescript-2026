dfc36b7edda39d697bca18ab625b726dec0bdfdb
Arun Motoori <arun.motoori@gmail.com>
2026-06-06 21:06:50 +0530

    Day 1 of Playwright TypeScript Automation

---
+++ Day1/demo.spec.ts
@@ -0,0 +1,21 @@
+import {chromium, test} from "@playwright/test";
+
+test("Get started with playwright test",async ()=>{
+   
+    const browser = await chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto("https://tutorialsninja.com/demo/");
+    await page.locator("//span[text()='My Account']").click();
+    await page.locator("//a[text()='Login']").click();
+    await page.locator("#input-email").fill("amotooricap6@gmail.com");
+    await page.locator("#input-password").fill("12345");
+    await page.locator("input[value='Login']").click();
+    await page.waitForTimeout(5000);
+
+}); 
+
+
+
+
